796ec016aafc69ad48863d11f6db65a9542907ec
sravankml <sravankml11@gmail.com>
2019-12-15 15:39:42 +0530

    user registration

--- app.py
+++ app.py
@@ -5,5 +5,14 @@ app = Flask(__name__)
 def index():
    return render_template('hello.html')
 
+@app.route('/register')
+def register():
+   return render_template('register.html')
+
+@app.route('/register/home')
+def home():
+   return_value = "<h1> You have been successfully registerd </h2>"
+   return return_value
+
 if __name__ == '__main__':
    app.run(debug = True)
